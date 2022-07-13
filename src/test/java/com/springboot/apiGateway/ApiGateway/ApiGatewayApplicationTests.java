38bf42069321a6def85af84f8c6d4b4a3c03c9ca
nkorde <nikhil.korde@capgemini.com>
2022-07-13 13:39:04 +0530

    Resilience 4j implementaion and config server

--- src/test/java/com/springboot/apiGateway/ApiGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/springboot/apiGateway/ApiGateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,30 @@
 package com.springboot.apiGateway.ApiGateway;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	private WebTestClient client;
+//
+//	@BeforeEach
+//	public void setup(){
+//		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + 8304).build();
+//	}
+//
+//	@Test
+//	public void pathRouteDb(){
+//		client.get().uri("/rest/db/Nikhil").exchange().expectStatus().isOk();
+//	}
+//
+//	@Test
+//	public void pathRouteStock(){
+//		client.get().uri("/rest/stock/Nikhil").exchange().expectStatus().isOk();
+//	}
+//
+
 
 }
