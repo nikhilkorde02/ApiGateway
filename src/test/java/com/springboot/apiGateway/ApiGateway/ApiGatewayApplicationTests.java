15772458a0a0330cacce60573fa16993cbc1a6f3
nkorde <nikhil.korde@capgemini.com>
2022-07-15 11:15:50 +0530

    Api Gateway test cases

--- src/test/java/com/springboot/apiGateway/ApiGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/springboot/apiGateway/ApiGateway/ApiGatewayApplicationTests.java
@@ -8,23 +8,23 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiGatewayApplicationTests {
 
-//	private WebTestClient client;
-//
-//	@BeforeEach
-//	public void setup(){
-//		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + 8304).build();
-//	}
-//
-//	@Test
-//	public void pathRouteDb(){
-//		client.get().uri("/rest/db/Nikhil").exchange().expectStatus().isOk();
-//	}
-//
-//	@Test
-//	public void pathRouteStock(){
-//		client.get().uri("/rest/stock/Nikhil").exchange().expectStatus().isOk();
-//	}
-//
+	private WebTestClient client;
+
+	@BeforeEach
+	public void setup(){
+		client = WebTestClient.bindToServer().baseUrl("http://localhost:" + 8304).build();
+	}
+
+	@Test
+	public void pathRouteDb(){
+		client.get().uri("/rest/db/Nikhil").exchange().expectStatus().isOk();
+	}
+
+	@Test
+	public void pathRouteStock(){
+		client.get().uri("/rest/stock/Nikhil").exchange().expectStatus().isOk();
+	}
+
 
 
 }
